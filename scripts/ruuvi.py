1522758dc6ee8d8e39a78f5c93d91584326c8549
Aleksi Sutela <asutela@gmail.com>
2023-10-29 10:49:15 +0200

    Increase python timeout

--- scripts/ruuvi.py
+++ scripts/ruuvi.py
@@ -30,7 +30,7 @@ else:
     print(f'{current_datetime} - No MAC addresses provided.')
 
 # This should be enough that we find at least one result for each
-timeout_in_sec = 60
+timeout_in_sec = 70
 
 url = "http://localhost:3001/api/ruuvi"
 
